86d41d2bc501f09ce6a8dadb1fd47f374c34e30a
PLarboulette <larboulette.pierre@gmail.com>
2017-11-23 21:15:57 +0100

    Adding validation of inputs in the endpoints

--- api/src/main/scala/routes/PublicationRoutes.scala
+++ api/src/main/scala/routes/PublicationRoutes.scala
@@ -1,32 +1,35 @@
 package routes
 import actors.PublicationActor.{FindAll, FindById}
 import akka.http.scaladsl.server.Directives.{complete, get, path, pathPrefix, _}
-import akka.http.scaladsl.server.Route
-import akka.util.Timeout
 import akka.actor.ActorRef
-import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Route
 import akka.util.Timeout
-import models.{Publication}
+import models.Publication
 import akka.pattern.ask
 import utils.MyJsonProtocol._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.unmarshalling.Unmarshaller
+import org.joda.time.DateTime
 
 import scala.concurrent.duration._
 
 object PublicationRoutes {
 
+  val stringToDateTime = Unmarshaller.strict[String, DateTime](DateTime.parse)
 
   def getRoutes (publicationActorRef : ActorRef) : Route = {
-    path("posts") {
+
+    path("api" / "posts") {
       get {
-        parameter("from".as[Option[String]], "to".as[Option[String]], "author".as[Option[String]]) {(from, to, author) =>
-          implicit val timeout : Timeout = 5.seconds
-          val posts = (publicationActorRef ? FindAll(from = from, to = to, author = author)).mapTo[List[Publication]]
-          complete(posts)
+        parameter("from".as(stringToDateTime).?, "to".as(stringToDateTime).?, "author".as[String].?) {(from, to, author) =>
+          complete {
+            implicit val timeout : Timeout = 5.seconds
+            val posts = (publicationActorRef ? FindAll(from = from.map(_.toString), to = to.map(_.toString), author = author)).mapTo[List[Publication]]
+            "OK"
+          }
         }
       }
-    } ~ pathPrefix("posts" / Segment ) { id =>
+    } ~ pathPrefix("api" / "posts" / Segment ) { id =>
       get {
         implicit val timeout: Timeout = 5.seconds
         val post = (publicationActorRef ? FindById(id)).mapTo[Option[Publication]]
