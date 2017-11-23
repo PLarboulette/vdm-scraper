81ec847928483b045d5a34859824520b67925a60
PLarboulette <larboulette.pierre@gmail.com>
2017-11-23 21:15:34 +0100

    Adding validation of inputs in the endpoints

--- api/src/main/scala/actors/PublicationActor.scala
+++ api/src/main/scala/actors/PublicationActor.scala
@@ -1,7 +1,7 @@
 package actors
 
 import actors.PublicationActor.FindAll
-import akka.actor.{Actor, ActorLogging}
+import akka.actor.{Actor, ActorLogging, Props}
 
 
 class PublicationActor extends Actor with ActorLogging {
@@ -15,6 +15,8 @@ class PublicationActor extends Actor with ActorLogging {
 }
 
 object PublicationActor {
+
+  def props () = Props(new PublicationActor())
 
   case class FindAll (from : Option[String] = None, to : Option[String] = None, author : Option[String] = None)
   case class FindById (id : String)
