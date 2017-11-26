5b1d355b21bad9931e187b8d132b3b04b396124b
PLarboulette <larboulette.pierre@gmail.com>
2017-11-26 16:50:57 +0100

    Adding converters ton Publication model

--- api/src/main/scala/models/Publication.scala
+++ api/src/main/scala/models/Publication.scala
@@ -1,6 +1,30 @@
 package models
 
+import org.mongodb.scala.bson.collection.Document
+
 case class Publication(id : String, content : Option[String], date : Option[Long], author : Option[String])
 {
+
+}
+
+object Publication {
+
+  implicit def toDocument (publication : Publication) : Document = {
+    Document(
+      "_id" -> publication.id,
+      "content" -> publication.content,
+      "date" -> publication.date,
+      "author" -> publication.author
+    )
+  }
+
+  implicit def toPublication (document : Document) : Publication = {
+    Publication(
+      document.get("_id").map(_.asString().getValue).getOrElse(""),
+      document.get("content").map(_.asString().getValue),
+      document.get("date").map(_.asDouble().getValue.toLong),
+      document.get("author").map(_.asString().getValue)
+    )
+  }
 
 }
