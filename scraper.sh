57f1f29f23d296943bc865c6c31366afa936917b
Pierre <larboulette.pierre@gmail.com>
2017-11-29 08:29:54 +0100

    API : find limited to 200 results and scraper.sh, adding name for container (it's better if we want to stop it with it's name)

--- scraper.sh
+++ scraper.sh
@@ -4,7 +4,7 @@ sbt scraper/docker:publishLocal
 echo "[docker:publishLocal] OK"
 echo "-----------------------"
 echo "[scraper] START"
-docker-compose run scraper
+docker-compose run --name vdm-scraper scraper
 echo "[scraper] OK"
 docker stop vdm-scraper && docker rm vdm-scraper
 read -p "Press any key to continue... " -n1 -s
