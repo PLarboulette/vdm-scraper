862c67d3c58c05b50d22359737ca6e7ef4459add
PLarboulette <larboulette.pierre@gmail.com>
2017-11-30 11:10:24 +0100

    Adding documentation on functions

--- api/src/main/scala/database/MongoHelper.scala
+++ api/src/main/scala/database/MongoHelper.scala
@@ -8,12 +8,30 @@ import scala.concurrent.{ExecutionContext, Future}
 
 object MongoHelper {
 
+  /**
+    * Helper find
+    * @param fn Function to convert Document to model
+    * @param filter List of filters
+    * @param coll
+    * @param ec
+    * @tparam T Type of Document (here Post)
+    * @return a list of T (Here Post)
+    */
    def find[T](fn : Document => T, filter : Option[Bson])(implicit coll : MongoCollection[Document], ec : ExecutionContext): Future[List[T]] = {
      for {
       documents <- filter.map(filters => coll.find(filters)).getOrElse(coll.find()).toFuture().recoverWith { case e : Throwable => Future.failed(e)}
     } yield documents.toList.map(fn)
   }
 
+  /**
+    * Helper findById
+    * @param id the id of the asked document
+    * @param fn a function to convert Document to model (here Post)
+    * @param coll
+    * @param ec
+    * @tparam T ype of Document (here Post)
+    * @return an option of T
+    */
   def findById[T](id : String, fn : Document => T)(implicit coll : MongoCollection[Document], ec: ExecutionContext): Future[Option[T]] = {
     for {
       documents <- coll.find(equal("_id", id)).toFuture().recoverWith{
@@ -22,6 +40,13 @@ object MongoHelper {
     } yield documents.toList.map(fn).headOption
   }
 
+  /**
+    * Helper cleanDB
+    * @param coll
+    * @param ec
+    * @tparam T T ype of Document (here Post)
+    * @return a completed statement
+    */
   def cleanDb[T] () (implicit coll: MongoCollection[Document], ec: ExecutionContext): Future[Completed] = {
     coll.drop().toFuture()
   }
--- api/src/main/scala/services/PostService.scala
+++ api/src/main/scala/services/PostService.scala
@@ -12,7 +12,15 @@ import scala.concurrent.{ExecutionContext, Future}
 
 object PostService {
 
-
+  /**
+    *
+    * @param from a from date to filter results
+    * @param to a to date to filter results
+    * @param author an author to filter sresults
+    * @param coll
+    * @param ec
+    * @return a list of posts eventually filtered
+    */
   def findAll (from : Option[String] = None, to : Option[String] = None, author : Option[String] = None) (implicit coll : MongoCollection[Document], ec : ExecutionContext) : Future[List[Post]] = {
 
     def addFilter (filter : Option[Bson], old : Bson): Bson = {
@@ -31,10 +39,23 @@ object PostService {
     }
   }
 
+  /**
+    * Find a post by ud
+    * @param id the id of the asked post
+    * @param coll
+    * @param ec
+    * @return the post with the id taken in parameter
+    */
   def findById (id : String) (implicit coll : MongoCollection[Document], ec : ExecutionContext) : Future[Option[Post]] = {
     MongoHelper.findById[Post](id, Post.toPost)
   }
 
+  /**
+    * Clean the DB
+    * @param coll
+    * @param ec
+    * @return a completed statement
+    */
   def cleanDb () (implicit coll : MongoCollection[Document], ec : ExecutionContext) : Future[Completed] = {
     MongoHelper.cleanDb[Post]()
   }
